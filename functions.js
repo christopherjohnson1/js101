b12c204e51321d92d3f6c04fd66d55afae7537ed
Chris Johnson <christopherjohnson1@gmail.com>
2020-01-20 18:51:40 -0600

    added dog breed function

--- functions.js
+++ functions.js
@@ -11,3 +11,10 @@ console.log(nuggetizer('pig'));
 console.log(nuggetizer('cow'));
 console.log(nuggetizer('fish'));
 console.log(nuggetizer('tire'));
+
+const dogBreed = (dog) => {
+    return `My favorite dog breed is ${dog}`;
+};
+
+console.log(dogBreed('lab'));
+console.log(dogBreed('actually a cat'));
