792521225e4d8a9fced6358abd2c307b72984634
Chris Johnson <christopherjohnson1@gmail.com>
2020-01-20 20:43:59 -0600

    added more objects

--- objects.js
+++ objects.js
@@ -20,3 +20,49 @@ const ageCheck = (age) => {
 
 ageCheck(12); // 'no drinks for you'
 ageCheck(24); // 'Party!!!!!!!'
+
+
+const expense = {
+    dateCreated: '01/17/2020',
+    location: 'Baja Burrito',
+    cost: 12.57,
+    isGood: true,
+};
+
+//dot notation
+console.log(expense.cost);
+
+//bracket notation
+console.log(expense['cost']); //12.57
+
+const employee = {
+    firstName: 'Chris',
+    lastName: 'Johnson',
+    age: 34,
+    section: 'Software Developer',
+    timeEmployeed: '2 years',
+    passDrugScreen: true,
+    salary: 3000000000,
+    collegeDegree: true,
+
+};
+
+console.log(employee.lastName); //Johnson
+
+
+// Challenge
+// Create function called statusMaker that takes in a single employee
+// if the employee firstName starts with an M then add a key of status and a value of 'vip'
+// if the employee firstName doesn't start with an M add a key of status and a value of 'peasant'
+// return complete employee
+
+const statusMaker = (emp) => {
+    if (emp.firstName[0] === 'M') {
+    emp.status = 'vip';
+    } else {
+        emp.status = 'peasant';
+    }
+    return emp;
+};
+
+console.log(statusMaker(employee));
