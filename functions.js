94c2452817557056815f97faebbb92341c64e9c4
Chris Johnson <christopherjohnson1@gmail.com>
2020-01-20 18:47:47 -0600

    wrote nuggetizer function

---
+++ functions.js
@@ -0,0 +1,13 @@
+console.log('Functions');
+
+const nuggetizer = (animal) => {
+    //console.log('animal', animal);
+    const output = `processed ${animal}`;
+    return output;
+};
+
+console.log(nuggetizer('chicken'));
+console.log(nuggetizer('pig'));
+console.log(nuggetizer('cow'));
+console.log(nuggetizer('fish'));
+console.log(nuggetizer('tire'));
